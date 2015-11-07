d814ac12519e548fb51d4b50b62142e514c9788d
ZackMisso <zack441@mac.com>
2015-11-07 17:29:04 -0600

    Fixed .h file

--- TypeInference.h
+++ TypeInference.h
@@ -10,16 +10,17 @@ class Expression;
 class AstBinOp;
 class AstUnOp;
 class AstExpressionList;
+class Type;
 
 class TypeInference {
 private:
 	Expression* program;
 public:
 	TypeInference(Expression* p);
-	void infer(Expression* e);
-	void infer_binop(AstBinOp *e);
-	void infer_unop(AstUnOp *e);
-	void infer_expression_list(AstExpressionList* l);
+	Type* infer(Expression* e);
+	Type* infer_binop(AstBinOp *e);
+	Type* infer_unop(AstUnOp *e);
+	Type* infer_expression_list(AstExpressionList* l);
 };
 
 #endif /* TYPE_INFERENCE_H_ */
